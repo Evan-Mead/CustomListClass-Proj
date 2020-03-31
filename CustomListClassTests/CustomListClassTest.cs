13668ebc46cbacf9745d85fdfedccb30eb893484
unknown <evan.mead@yahoo.com>
2020-03-31 07:44:28 -0500

    Added to, and rearranged CLCT

--- CustomListClassTests/CustomListClassTest.cs
+++ CustomListClassTests/CustomListClassTest.cs
@@ -15,10 +15,72 @@ namespace CustomListClassTests
             int actual;
 
             // act
-            numbers.Add(11);
+            numbers.Add(1);
             actual = numbers.Count;
 
             // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTwo_CheckCountTwo()
+        {
+            CustomList<int> numbers = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            numbers.Add(1);
+            numbers.Add(2);
+            actual = numbers.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddThree_CheckCountThree()
+        {
+            CustomList<int> numbers = new CustomList<int>();
+            int expected = 3;
+            int actual;
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            actual = numbers.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddFour_CheckCountFour()
+        {
+            CustomList<int> numbers = new CustomList<int>();
+            int expected = 4;
+            int actual;
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            actual = numbers.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddFive_CheckCountFive()
+        {
+            CustomList<int> numbers = new CustomList<int>();
+            int expected = 5;
+            int actual;
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+            actual = numbers.Count;
+
             Assert.AreEqual(expected, actual);
         }
 
@@ -27,31 +89,61 @@ namespace CustomListClassTests
         {
             // arrange
             CustomList<int> numbers = new CustomList<int>();
-            int expected = 17;
+            int expected = 1;
             int actual;
 
-
             // act
-            numbers.Add(17);
+            numbers.Add(1);
             actual = numbers[0];
-
 
             // assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void AddOne_CheckCapacity()
+        public void AddTwo_CheckIndexOne()
         {
-            CustomList<string> words = new CustomList<string>();
+            CustomList<int> numbers = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            numbers.Add(1);
+            numbers.Add(2);
+            actual = numbers[1];
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddThree_CheckIndexTwo()
+        {
+            CustomList<int> numbers = new CustomList<int>();
+            int expected = 3;
+            int actual;
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            actual = numbers[2];
+
+            Assert.AreEqual.(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddFour_CheckIndexThree()
+        {
+            CustomList<int> numbers = new CustomList<int>();
             int expected = 4;
             int actual;
 
-            words.Add("hello");
-            actual = words.Capacity;
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            actual = numbers[3];
 
-            Assert.AreEqual(expected, actual);
-        }                                       
+            Assert.AreEqual.(expected, actual);
+        }
 
         [TestMethod]
         public void AddFive_CheckIndexFour()
@@ -60,13 +152,73 @@ namespace CustomListClassTests
             int expected = 5;
             int actual;
 
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
             numbers.Add(5);
-            numbers.Add(10);
-            numbers.Add(15);
-            numbers.Add(20);
-            numbers.Add(25);
-
             actual = numbers[4];
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddOne_CheckCapacity()
+        {
+            // arrange
+            CustomList<string> words = new CustomList<string>();
+            int expected = 4;
+            int actual;
+
+            // act
+            words.Add("hello");
+            actual = words.Capacity;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTwo_CheckCapacity()
+        {
+            CustomList<string> words = new CustomList<string>();
+            int expected = 4;
+            int actual;
+
+            words.Add("hello");
+            words.Add("how");
+            actual = words.Capacity;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Addhree_CheckCapacity()
+        {
+            CustomList<string> words = new CustomList<string>();
+            int expected = 4;
+            int actual;
+
+            words.Add("hello");
+            words.Add("how");
+            words.Add("are");
+            actual = words.Capacity;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddFour_CheckCapacity()
+        {
+            CustomList<string> words = new CustomList<string>();
+            int expected = 4;
+            int actual;
+
+            words.Add("hello");
+            words.Add("how");
+            words.Add("are");
+            words.Add("you");
+            actual = words.Capacity;
 
             Assert.AreEqual(expected, actual);
         }
@@ -75,7 +227,7 @@ namespace CustomListClassTests
         public void AddFive_CheckCapacity()
         {
             CustomList<string> words = new CustomList<string>();
-            int expected = 4;
+            int expected = 8;
             int actual;
 
             words.Add("hello");
@@ -87,7 +239,6 @@ namespace CustomListClassTests
 
             Assert.AreEqual(expected, actual);
         }
-
 
     }
 }
